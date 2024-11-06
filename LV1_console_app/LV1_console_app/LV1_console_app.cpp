07402ab64a5fd74e9beef5fe88175a4ab92ebad7
KarloStefanac <karlostef6@gmail.com>
2024-11-06 09:17:35 +0100

    Initial commit

---
+++ LV1_console_app/LV1_console_app/LV1_console_app.cpp
@@ -0,0 +1,10 @@
+// LV1_console_app.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Pozdrav iz aplikacije od Karlo Štefanac\n";
+}
+
